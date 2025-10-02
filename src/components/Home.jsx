6c1383689f4b32bffbdcde3c2445c556d4cc8f6d
vickysrana2-cloud <vickysrana2@gmail.com>
2025-10-03 01:30:30 +0530

    added a proper hero section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import "../app.css";
+import { Link } from "react-router";
 
 const Home = () => {
   return (
     <>
+      <div className="upperhero rounded-t-3xl mt-2">
+        <div className="circle circle1"></div>
+        <div className="circle circle2"></div>
+        <div className="circle circle3"></div>
+        <div className="circle circle4"></div>
+        <div className="circle circle5"></div>
+
+        <div className="text-center lg:text-left max-w-5xl lg:-mt-28 -mt-64 font-['Times_New_Roman']">
+          <h1 className="text-4xl lg:text-5xl font-bold text-white mb-4">
+            Hi there,
+          </h1>
+          <h1 className="text-3xl lg:text-8xl font-bold text-orange-600 text-shadow-2xs text-shadow-orange-300 mb-6">
+            I’m Vicky Rana
+          </h1>
+          <p className="text-gray-300 font-semibold lg:text-lg mb-8">
+            Web Developer | Building Modern, Responsive Web Apps
+          </p>
+          <Link
+            to={"/projects"}
+            className="relative overflow-hidden px-6 py-3 font-bold text-white rounded-full backdrop-blur-lg bg-white/10 group"
+          >
+            <span className="relative z-10 rounded-full">View My Projects</span>
+            <span className="absolute rounded-full inset-0 w-full h-full bg-gradient-to-r from-pink-500 to-yellow-500 transform scale-x-0 origin-center transition-transform duration-300 ease-out group-hover:scale-x-100"></span>
+          </Link>
+        </div>
+      </div>
+
       <section
-        className="w-full min-h-screen bg-gradient-to-r rounded-3xl my-2 from-black/50  via-gray-700 to-teal-800
+        className="w-full min-h-screen linierEffect 
            flex items-center justify-center px-6 lg:px-32"
       >
         <div className="text-center lg:text-left max-w-3xl lg:-mt-28 -mt-64">
@@ -23,7 +52,6 @@ const Home = () => {
             building engaging, efficient, and meaningful web applications.
             Scroll down to check out some of my work!
           </p>
-
         </div>
       </section>
 
@@ -33,7 +61,8 @@ const Home = () => {
             🚧 Work in Progress
           </h2>
           <p className="text-gray-600 text-base lg:text-lg leading-relaxed mb-6">
-            Currently under development. Exciting additions will be available here soon.
+            Currently under development. Exciting additions will be available
+            here soon.
           </p>
           <div className="flex items-center justify-center gap-3 text-teal-600 font-medium">
             <span className="w-3 h-3 bg-teal-500 rounded-full animate-pulse"></span>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-black rounded-b-2xl text-white px-6 py-4 shadow-md sticky top-0 z-50">
+    <nav className=" bg-black/60 backdrop-blur-xs border-b-2 border-b-white/10 rounded-b-3xl text-white font-semibold px-6 py-4 shadow-md sticky top-0 z-50">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         {/* Left: Logo / Name */}
 
@@ -15,14 +15,16 @@ const Navbar = () => {
           to={"/"}
           className="flex items-center lg:text-4xl text-2xl font-bold tracking-wide transition-all duration-200 ease-in-out
              [text-shadow:2px_2px_4px_rgba(220,38,38,0.5)] 
-             hover:[text-shadow:0_0_15px_rgba(220,38,38,1),_0_0_20px_rgba(220,38,38,1)]"
+             hover:[text-shadow:0_0_5px_rgba(220,38,38,1),_0_0_5px_rgba(220,38,38,1)]"
         >
           {"<VickyRana/>"}
         </Link>
 
         {/* Desktop Menu (only on lg and above) */}
         <div className="hidden lg:flex items-center space-x-8">
-          <Link to={"/projects"} className="hover:text-cyan-400 transition" >PROJECT-S</Link>
+          <Link to={"/projects"} className="hover:text-cyan-400 transition">
+            PROJECT-S
+          </Link>
           <Link to={"/aboutMe"} className="hover:text-cyan-400 transition">
             ABOUT ME
           </Link>
@@ -58,25 +60,39 @@ const Navbar = () => {
 
       {/* Mobile/Tablet Dropdown */}
       {isOpen && (
-        <div className="lg:hidden mt-4  flex flex-col space-y-4 bg-gray-900 p-4 rounded-2xl">
-          <Link to={"/projects"} className="hover:text-cyan-400 transition">
+        <div className="lg:hidden mt-4 flex flex-col space-y-4 bg-gray-900 p-4 rounded-2xl">
+          <Link
+            to={"/projects"}
+            className="hover:text-cyan-400 transition"
+            onClick={() => setIsOpen(false)}
+          >
             PROJECT-S
           </Link>
-          <Link to={"/aboutMe"} className="hover:text-cyan-400 transition">
+          <Link
+            to={"/aboutMe"}
+            className="hover:text-cyan-400 transition"
+            onClick={() => setIsOpen(false)}
+          >
             ABOUT ME
           </Link>
-          <Link to={"/specialties"} className="hover:text-cyan-400 transition">
+          <Link
+            to={"/specialties"}
+            className="hover:text-cyan-400 transition"
+            onClick={() => setIsOpen(false)}
+          >
             SPECIALTIES
           </Link>
           <Link
             to={"/contact"}
-            className="hover:text-cyan-400 text-sm flex items-center  tracking-wide transition"
+            className="hover:text-cyan-400 text-sm flex items-center tracking-wide transition"
+            onClick={() => setIsOpen(false)}
           >
             CONTACT <GoArrowUpRight className="ms-0.5" />
           </Link>
           <Link
             to={"/resume"}
             className="bg-gray-800 hover:bg-gray-700 flex items-center justify-center hover:text-cyan-400 text-white px-4 py-2 rounded-full font-semibold text-sm tracking-wider text-center transition"
+            onClick={() => setIsOpen(false)}
           >
             MY RESUME <GoArrowUpRight className="ms-0.5" />
           </Link>
